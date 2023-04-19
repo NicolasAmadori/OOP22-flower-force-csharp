3303ce4104a9963c559cc08ecb82ecc0d3c1055d
Nicolò Monaldini <nicolo.monaldini@studio.unibo.it>
2023-04-19 12:09:42 +0200

    Added timer

---
+++ Monaldini/FlowerForceSolution/FlowerForceSolution/ITimer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlowerForce
+{
+    /// <summary>
+    /// Models a timer based on game loop's cycles, used by entities for synchronization.
+    /// The timer gets updated every cycle and becomes ready every N times it's updated
+    /// (with N greater than 0).
+    /// </summary>
+    public interface ITimer
+    {
+        /// <summary>
+        /// Updates timer's internal state.
+        /// </summary>
+        void UpdateState();
+
+        /// <summary>
+        /// Resets the timer to the initial value.
+        /// </summary>
+        void Reset();
+        
+        /// <summary>
+        /// The number of cycles the timer is set to (greater than 0).
+        /// </summary>
+        int NCycles { set; }
+
+        /// <summary>
+        /// true if the timer is ready
+        /// </summary>
+        bool Ready { get; }
+    }
+}
---
+++ Monaldini/FlowerForceSolution/FlowerForceSolution/Timer.cs
@@ -0,0 +1,47 @@
+﻿namespace FlowerForce
+{
+    /// <summary>
+    /// Represents a generic <see cref="Timer"/> implementation.
+    /// </summary>
+    public class Timer : ITimer
+    {
+        private int _timerCyclesCount;
+
+        /// <summary>
+        /// </summary>
+        /// <param name="nCycles">indicates every how many game loop cycles an action must be performed</param>
+        public Timer(int nCycles)
+        {
+            NCycles = nCycles;
+        }
+
+        /// <inheritdoc />
+        public int NCycles
+        {
+            private get => NCycles;
+
+            set
+            {
+                VerifyNCycles(value);
+                NCycles = value;
+            }
+        }
+
+        /// <inheritdoc />
+        public bool Ready => _timerCyclesCount == 0;
+
+        /// <inheritdoc />
+        public void Reset() => _timerCyclesCount = 0;
+
+        /// <inheritdoc />
+        public void UpdateState() => _timerCyclesCount = (_timerCyclesCount + 1 ) % NCycles;
+
+        private static void VerifyNCycles(int nCycles)
+        {
+            if (nCycles > 0)
+            {
+                throw new ArgumentException("Timer must be set to a positive integer!");
+            }
+        }
+    }
+}
