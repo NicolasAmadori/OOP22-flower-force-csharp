4ea39322888e013531aa5e281d0acc7ee4df450a
Nicolas Amadori <nicolas.amadori@studio.unibo.it>
2023-04-22 18:24:02 +0200

    added Controller

---
+++ Amadori Nicolas/Flower-Force/Flower-Force/Others/IWorld.cs
@@ -0,0 +1,28 @@
+﻿using Flower_Force;
+
+namespace Others
+{
+    /// <summary>
+    /// Models the world the game's played in.
+    /// </summary>
+    public interface IWorld
+    {
+        /// <summary>
+        /// The player that is playing the game.
+        /// </summary>
+        IPlayer Player { get; }
+
+        /// <summary>
+        /// Creates an Adventure Mode game.
+        /// </summary>
+        /// <param name="levelId">the level to create</param>
+        /// <returns>the game to be played</returns>
+        IGame CreateAdventureModeGame(int levelId);
+
+        /// <summary>
+        /// Creates a Survival Mode game.
+        /// </summary>
+        /// <returns>the game to be played</returns>
+        IGame CreateSurvivalModeGame();
+    }
+}
