fc4c5140977c1bfce83ce436858b9ba5a38def9b
Nicolas Amadori <nicolas.amadori@studio.unibo.it>
2023-04-23 11:35:56 +0200

    reinserted cs files

---
+++ Amadori/FlowerForceSolution/FlowerForceSolution/Controller.cs
@@ -0,0 +1,51 @@
+﻿using Others;
+
+namespace Flower_Force
+{
+    /// <summary>
+    /// This is an implementation of <see cref="IController"/>.
+    /// </summary>
+    class Controller : IController
+    {
+        private IWorld _world;
+        private IGame _game;
+
+        /// <inheritdoc />
+        public int PlayerCoins
+        {
+            get => _world.Player.Coins;
+        }
+
+        /// <inheritdoc />
+        public int PlayerScoreRecord
+        {
+            get => _world.Player.ScoreRecord;
+        }
+
+        /// <inheritdoc />
+        public int LastUnlockedLevel
+        {
+            get => _world.Player.LastUnlockedLevelId;
+        }
+
+        /// <summary>
+        /// Create a new instance of Controller.
+        /// </summary>
+        public Controller()
+        {
+            _world = new World();
+        }
+
+        /// <inheritdoc />
+        public void StartNewAdventureModeGame(int levelId)
+        {
+            _game = _world.CreateAdventureModeGame(levelId);
+        }
+
+        /// <inheritdoc />
+        public void StartNewSurvivalModeGame()
+        {
+            _game = _world.CreateSurvivalModeGame();
+        }
+    }
+}
---
+++ Amadori/FlowerForceSolution/FlowerForceSolution/IController.cs
@@ -0,0 +1,34 @@
+﻿namespace Flower_Force
+{
+    /// <summary>
+    /// This is the class that connects the view and the model sides, making them communicate.
+    /// </summary>
+    public interface IController
+    {
+        /// <summary>
+        /// Get the number of coins the player has.
+        /// </summary>
+        int PlayerCoins { get; }
+
+        /// <summary>
+        /// Get the score record the player has obtained.
+        /// </summary>
+        int PlayerScoreRecord { get; }
+
+        /// <summary>
+        /// Get the id of the last level the player has unlocked.
+        /// </summary>
+        int LastUnlockedLevel { get; }
+
+        /// <summary>
+        /// Start a new game for a specified level.
+        /// </summary>
+        /// <param name="levelId">The id of the level to play</param>
+        void StartNewAdventureModeGame(int levelId);
+
+        /// <summary>
+        /// Start a new game in survival mode.
+        /// </summary>
+        void StartNewSurvivalModeGame();
+    }
+}
