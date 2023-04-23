844cd29efcab45d37900507f6d0c88ecf0bbf945
Nicolò Monaldini <nicolo.monaldini@studio.unibo.it>
2023-04-23 14:57:31 +0200

    Added documentation

--- Monaldini/FlowerForceSolution/FlowerForceSolution/Others/IZombie.cs
+++ Monaldini/FlowerForceSolution/FlowerForceSolution/Others/IZombie.cs
@@ -2,14 +2,32 @@
 
 namespace Others
 {
+    /// <summary>
+    /// Models a zombie that is both <see cref="IMovingEntity"/> and <see cref="ILivingEntity">.
+    /// </summary>
     public interface IZombie : IMovingEntity, ILivingEntity
     {
+        /// <summary>
+        /// Slows down the zombie
+        /// </summary>
         void Freeze();
 
+        /// <summary>
+        /// Restore the original velocity of the zombie
+        /// </summary>
         void WarmUp();
 
+        /// <summary>
+        /// This method can be called to manage eating a plant, indeed it will be the zombie
+        /// to decide if bit it or not
+        /// </summary>
+        /// <param name="plant"> that can be eaten by the zombie </param>
+        /// <returns> true if the zombie has bitten the plant, false otherwise </returns>
         bool ManageEating(IPlant plant);
 
+        /// <summary>
+        /// </summary>
+        /// <value> The generic difficulty of the zombie </value>
         int Difficulty { get; }
     }
 }
--- Monaldini/FlowerForceSolution/FlowerForceSolution/Others/YardInfo.cs
+++ Monaldini/FlowerForceSolution/FlowerForceSolution/Others/YardInfo.cs
@@ -2,12 +2,28 @@
 
 namespace Others
 {
+    /// <summary>
+    /// Models the yard of the game, in which the entities are placed.
+    /// </summary>
     public static class YardInfo
     {
+        /// <summary>
+        /// The rows of yard's cells matrix
+        /// </summary>
         public const int Rows = 5;
+
+        /// <summary>
+        /// The columns of yard's cells matrix
+        /// </summary>
         public const int Cols = 9;
+
         private const float Height = 880f;
+
         private const float Width = 1314f;
+
+        /// <summary>
+        /// The dimension of a single cell
+        /// </summary>
         public static readonly SizeF Cell = new SizeF(Width / Cols, Height / Rows);
     }
 }
